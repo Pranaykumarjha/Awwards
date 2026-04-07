d6d5cad27271099fdcf8b03c4674fa55439bd335
Pranay Kumar Jha <pranayjha952@gmail.com>
2026-04-07 23:12:21 +0530

    Added a footer

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
 import gsap from 'gsap';
 import RoundedCorners from './RoundedCorners';
+import Button from './Button';
 const Story = () => {
     const frameRef = useRef('null');
     const handleMouseLeave = () => {
@@ -66,15 +67,24 @@ const Story = () => {
                                 alt="entrance"
                                 className="object-contain"
                             />
-                            <RoundedCorners/>
+                            <RoundedCorners />
 
                         </div>
 
                     </div>
-                    <div>
-                        
-                    </div>
                 </div>
+                <div className='-mt-80 flex w-full justify-center md:mt-64 md:me-44 md:justify-end'>
+                    <div className="flex h-full w-fit flex-col items-center md:items-start">
+                        <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start'>Where realms converge, lies Zentry and the boundless pillar.
+                            Discover its secrets and shape your fate amidst infinite opportunities.
+
+                            <Button id="realm-button" title="discover prologue" containerClass="mt-5"
+                            />
+                        </p>
+                    </div>
+
+                </div>
+
 
 
             </div>
